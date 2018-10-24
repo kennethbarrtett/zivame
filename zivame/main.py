8a8fb8a27b53bdc29a8ca5473f60920a8f20ec18
kennethbarrtett <44135632+kennethbarrtett@users.noreply.github.com>
2018-10-24 14:29:37 -0400

    Update main.py

--- zivame/main.py
+++ zivame/main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect("https://www.zivame.com", True)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
