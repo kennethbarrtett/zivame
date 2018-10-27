6276d42bdebc638fd7944ac22fac56ef6673de4b
kennethbarrtett <44135632+kennethbarrtett@users.noreply.github.com>
2018-10-27 01:37:06 -0400

    Update main.py

--- zivame/main.py
+++ zivame/main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.redirect("https://www.zivame.com", True)
+        self.redirect("http://tracking.vcommission.com/aff_c?offer_id=100&aff_id=77927", True)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
